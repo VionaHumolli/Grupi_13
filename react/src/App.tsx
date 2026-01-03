e43cdef8848a1e0a7c21760983f1f584db4e8613
Omujaj <oltimujaj00@gmail.com>
2026-01-03 16:33:19 +0100

    Create App component with ThankYouPage

---
+++ react/src/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ThankYouPage from './ThankYouPage'; 
+
+function App() {
+  return (
+    <div className="App">
+      <ThankYouPage />
+    </div>
+  );
+}
+
+export default App;
